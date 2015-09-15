80e7291da1c3ed47563e7319dda3b186b0211d88
buidanhquy <quy.buidanhquy@gmail.com>
2015-09-15 17:06:00 +0700

    done riuc

---
+++ Makefile
@@ -0,0 +1,98 @@
+.PHONY: all clean test doc
+
+APP:=riuc
+TESTS:=riuc.c
+
+DIR:=.
+PROTOCOLS_DIR:=../protocols
+
+ICS_DIR:=../ics
+ICS_SRCS:=ics.c ics-event.c ics-command.c
+
+C_DIR:=../common
+C_SRCS:=ansi-utils.c
+
+GEN_DIR:=./gen
+GEN_C_SRCS:=arbiter-client.c
+GEN_O_SRCS:=oiu-client.c
+GEN_S_SRCS:=riu-server.c
+
+Q_DIR:=../concurrent_queue
+Q_SRCS:=queue.c
+
+O_DIR:=../object-pool
+O_SRCS:=object-pool.c
+
+USERVER_DIR:=../userver
+ARBITER_DIR:=../arbiter
+
+SERIAL_DIR := ../serial
+SERIAL_SRCS := riuc4_uart.c serial_utils.c
+
+CFLAGS:=-std=c99 $(shell pkg-config --cflags libpjproject)
+CFLAGS+=-I$(ICS_DIR)/include -I$(Q_DIR)/include -I$(O_DIR)/include
+CFLAGS+=-I$(C_DIR)/include -I$(USERVER_DIR)/include
+CFLAGS+= -I../json-c/output/include/json-c
+CFLAGS+= -I../arbiter/include
+CFLAGS+= -D_GNU_SOURCE
+CFLAGS += -I$(GEN_DIR) -I$(SERIAL_DIR)/include
+
+LIBS:=$(shell pkg-config --libs libpjproject) ../json-c/output/lib/libjson-c.a -lpthread
+
+A_PROTOCOL:=arbiter_proto.u
+O_PROTOCOL:=oiu_proto.u
+U_PROTOCOL:=riu_proto.u
+
+all: gen-a gen-o gen-u $(APP)
+
+gen-a: $(PROTOCOLS_DIR)/$(A_PROTOCOL)
+	mkdir -p gen
+	awk -f $(USERVER_DIR)/gen-tools/gen.awk $(PROTOCOLS_DIR)/arbiter_proto.u
+
+gen-o: $(PROTOCOLS_DIR)/$(O_PROTOCOL)
+	mkdir -p gen
+	awk -f $(USERVER_DIR)/gen-tools/gen.awk $(PROTOCOLS_DIR)/oiu_proto.u
+
+gen-u: $(PROTOCOLS_DIR)/$(U_PROTOCOL)
+	mkdir -p gen
+	awk -f $(USERVER_DIR)/gen-tools/gen.awk $(PROTOCOLS_DIR)/riu_proto.u
+
+doc: html latex
+	doxygen
+html:
+	mkdir -p $@
+latex:
+	mkdir -p $@
+
+$(APP): $(SRCS:.c=.o) $(TESTS:.c=.o) $(Q_SRCS:.c=.o) $(O_SRCS:.c=.o) $(C_SRCS:.c=.o) $(USERVER_C_SRCS:.c=.o) $(GEN_S_SRCS:.c=.o) $(GEN_O_SRCS:.c=.o) $(GEN_C_SRCS:.c=.o) $(ICS_SRCS:.c=.o) $(SERIAL_SRCS:.c=.o)
+	gcc -o $@ $^ $(LIBS)
+
+$(GEN_C_SRCS:.c=.o): %.o: $(GEN_DIR)/%.c
+	gcc -o $@ -c $< $(CFLAGS)
+
+$(GEN_O_SRCS:.c=.o): %.o: $(GEN_DIR)/%.c
+	gcc -o $@ -c $< $(CFLAGS)
+
+$(GEN_S_SRCS:.c=.o): %.o: $(GEN_DIR)/%.c
+	gcc -o $@ -c $< $(CFLAGS)
+
+$(TESTS:.c=.o): %.o: $(DIR)/src/%.c
+	gcc -c -o $@ $< $(CFLAGS)
+
+$(ICS_SRCS:.c=.o): %.o: $(ICS_DIR)/src/%.c
+	gcc -c -o $@ $< $(CFLAGS)
+$(O_SRCS:.c=.o): %.o: $(O_DIR)/src/%.c
+	gcc -c -o $@ $< $(CFLAGS)
+
+$(Q_SRCS:.c=.o): %.o: $(Q_DIR)/src/%.c
+	gcc -c -o $@ $< $(CFLAGS)
+$(C_SRCS:.c=.o): %.o: $(C_DIR)/src/%.c
+	gcc -c -o $@ $< $(CFLAGS)
+$(USERVER_C_SRCS:.c=.o): %.o: $(USERVER_DIR)/src/%.c
+	gcc -o $@ -c $< $(CFLAGS)
+
+$(SERIAL_SRCS:.c=.o) : %.o : $(SERIAL_DIR)/src/%.c
+	$(CC) -o $@ -c $< $(CFLAGS)
+
+clean:
+	rm -fr *.o $(APP) $(LOG) html latex gen
---
+++ src/riuc.c
@@ -0,0 +1,222 @@
+#include "riuc4_uart.h"
+#include "ansi-utils.h"
+
+#include <pjsua-lib/pjsua.h>
+#include <unistd.h>
+
+#include "ics.h"
+#include "arbiter-client.h"
+#include "oiu-client.h"
+#include "riu-server.h"
+
+struct {
+    ics_t ics_data;
+    arbiter_client_t aclient;
+    oiu_client_t oclient;
+    riu_server_t rserver;
+} app_data;
+
+//Callback function for Arbiter
+int arbiter_send(arbiter_client_t *uclient, arbiter_request_t *req) {
+	return arbiter_client_send(uclient, req);
+}
+
+void * auto_send_to_arbiter(void * arg) {
+    arbiter_request_t req;
+
+    arbiter_client_t *aclient = (arbiter_client_t *) arg;
+    
+    req.msg_id = ABT_UP;
+    strncpy(req.abt_up.type, "RIU", sizeof(req.abt_up.type));
+    strncpy(req.abt_up.username, "RIUC1", sizeof(req.abt_up.username));
+    req.abt_up.n_ports = 4;
+    req.abt_up.code = 1;
+    while (1) {
+        arbiter_send(aclient, &req);
+        sleep(5);
+    }
+}
+void on_riuc4_status(int port, riuc4_signal_t signal, uart4_status_t *ustatus) {
+    fprintf(stdout, "RIUC4 port %d, update signal %s. Status: tx=%d,rx=%d,ptt=%d,sq=%d\n", port, RIUC4_SIGNAL_NAME[signal], ustatus->tx, ustatus->rx, ustatus->ptt, ustatus->sq);
+
+    if (signal == RIUC_SIGNAL_SQ) {
+        oiu_client_t *oclient = (oiu_client_t *)ustatus->user_data;
+
+        oiu_request_t req;
+        req.msg_id = OIUC_SQ;
+        strncpy(req.oiuc_sq._id, "RIUC1", sizeof(req.oiuc_sq._id));
+        req.oiuc_sq.port = port;
+        strncpy(req.oiuc_sq.multicast_addr,"udp:129.0.0.1:4321", sizeof(req.oiuc_sq.multicast_addr));
+        oiu_client_send(oclient, &req);
+    }    
+}
+
+static void on_request(riu_server_t *rserver, riu_request_t *req);
+
+int main(int argc, char *argv[]) {
+    char temp[10];
+    int i;
+    int fEnd = 0;
+    int port_idx;
+
+    pthread_t thread;
+
+    serial_t serial;
+    riuc4_t riuc4;
+
+    if (argc < 2) {
+        fprintf(stdout, "Usage: myapp-riuc4 <device> <request connect string> <listen connection string>\n");
+        exit(-1);
+    }
+    
+    fprintf(stdout, "RIUC - 4COM - RIUC-ccp-v0.2\n");
+
+    for (i = 0; i < N_RIUC; i++) {
+        riuc4.riuc4_status[i].user_data = &app_data.oclient;
+    }
+
+    riuc4_init(&serial, &riuc4, &on_riuc4_status);
+
+    riuc4_start(&serial, argv[1]);
+
+#if 0
+    //SEND
+    char sqa_mip[50] = "udp:239.0.0.1:12345";
+ 
+    arbiter_client_open(&app_data.aclient, argv[2]);
+    oiu_client_open(&app_data.oclient, sqa_mip);
+
+    //Arbiter path
+    // LISTEN
+    riu_server_init(&app_data.rserver, argv[3]);
+    riu_server_start(&app_data.rserver);
+
+    //End Arbiter path
+#endif
+
+#if 1
+   //SEND
+    char sqa_mip[] = "udp:239.0.0.1:1234";
+    char send_a[] = "udp:127.0.0.1:4321";
+    char recv[] = "udp:0.0.0.0:12345";
+    arbiter_client_open(&app_data.aclient, send_a);
+    oiu_client_open(&app_data.oclient, sqa_mip);
+
+    //Arbiter path
+    // LISTEN
+    app_data.rserver.user_data = &riuc4;
+
+    app_data.rserver.on_request_f = &on_request;
+    riu_server_init(&app_data.rserver, recv);
+    riu_server_start(&app_data.rserver);
+
+    //End Arbiter path
+#endif
+
+    pthread_create(&thread, NULL, auto_send_to_arbiter, &app_data.aclient);
+
+    
+    while(!fEnd) {
+        fprintf(stdout, "COMMAND:<port[1-4]><command[DdEertsl+-q]>:\n");
+        fgets(temp, sizeof(temp), stdin);
+        port_idx = temp[0] - '1';
+        if( port_idx < 0 || port_idx > 3 ) {
+            fprintf(stdout, "Command error\n");
+            continue;
+        }
+        switch (temp[1]) {
+            case 'D':
+                    riuc4_disable_rx(&riuc4, port_idx);
+                    break;
+            case 'd':
+                    riuc4_disable_tx(&riuc4, port_idx);
+                    break;
+            case 'E':
+                    riuc4_enable_rx(&riuc4, port_idx);
+                    break;
+            case 'e':
+                    riuc4_enable_tx(&riuc4, port_idx);
+                    break;
+            case 'r':
+                    riuc4_probe_rx(&riuc4, port_idx);
+                    break;
+            case 't':
+                    riuc4_probe_tx(&riuc4, port_idx);
+                    break;
+            case 's':
+                    riuc4_probe_sq(&riuc4, port_idx);
+                    break;
+            case 'l':
+                    riuc4_probe_ptt(&riuc4, port_idx);
+                    break;
+            case '+':
+                    riuc4_on_ptt(&riuc4, port_idx);
+                    break;
+            case '-':
+                    riuc4_off_ptt(&riuc4, port_idx);
+                    break;
+            case 'q':
+                    fEnd = 1;
+                    break;
+            default:
+                    fprintf(stdout, "Unknown command");
+                    break;
+        }
+    }  
+    pthread_join(thread, NULL);
+
+    riuc4_end(&serial);
+
+    return 0;
+}
+
+static void on_request(riu_server_t *rserver, riu_request_t *req) {
+    riuc4_t *riuc4 = (riuc4_t *)rserver->user_data;
+    int port_idx;
+    char temp[10];
+    strncpy(temp,req->riuc_ptt.msg, sizeof(temp));
+
+    port_idx = temp[0] - '1';
+
+    if( port_idx < 0 || port_idx > 3 ) {
+        fprintf(stdout, "Command error\n");
+    }
+    switch (temp[1]) {
+        case 'D':
+            riuc4_disable_rx(riuc4, port_idx);
+            break;
+        case 'd':
+            riuc4_disable_tx(riuc4, port_idx);
+            break;
+        case 'E':
+            riuc4_enable_rx(riuc4, port_idx);
+            break;
+        case 'e':
+            riuc4_enable_tx(riuc4, port_idx);
+            break;
+        case 'r':
+            riuc4_probe_rx(riuc4, port_idx);
+            break;
+        case 't':
+            riuc4_probe_tx(riuc4, port_idx);
+            break;
+        case 's':
+            riuc4_probe_sq(riuc4, port_idx);
+            break;
+        case 'l':
+            riuc4_probe_ptt(riuc4, port_idx);
+            break;
+        case '+':
+            riuc4_on_ptt(riuc4, port_idx);
+            break;
+        case '-':
+            riuc4_off_ptt(&riuc4, port_idx);
+            break;
+        case 'q':
+            break;
+        default:
+            fprintf(stdout, "Unknown command\n");
+            break;
+    }
+}
+
